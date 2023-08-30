44e4684d68c3fc758fec4d57753573182b0a40af
tehZevo <tehzevo@users.noreply.github.com>
2023-08-30 12:19:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name="gym_tetris_obs_wrapper",
     version="0.1.0",
     description="Wrapper for simpler gym-tetris observations",
-    install_requires=["gym-tetris"],
+    install_requires=[
+      "gym-tetris",
+      "skimage",
+      "gym<=0.18.3",
+      "numpy"
+    ],
 )
